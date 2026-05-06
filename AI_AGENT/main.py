67626b508e7e3566fd68babd9f6377dc6d675106
Ajay-v44 <vajay4834@gmail.com>
2026-05-06 21:08:01 +0530

    feat: implement restaurant review RAG system using ChromaDB, LangChain, and Ollama embeddings

---
+++ AI_AGENT/main.py
@@ -0,0 +1,34 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from embeding import retriever
+
+model=OllamaLLM(model="llama3.2")
+
+template="""
+    You are an exeprt in answering questions about a pizza restaurant
+
+    Here are some relevant reviews: {reviews}
+
+    Here is the question to answer: {question}
+    """
+
+
+prompt=ChatPromptTemplate.from_template(template)
+
+chain=prompt|model
+
+
+while True:
+    question = input("Enter your question: ")
+    if question=="q":
+        break
+    reviews=retriever.invoke(question)
+    result=chain.invoke(
+        {
+            "reviews": reviews,
+            "question": question
+            }
+        )
+    print(result)
+
+
