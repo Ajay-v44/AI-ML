b570f638781dff59afd5b81c15c7662ebf094d84
Ajay-v44 <vajay4834@gmail.com>
2026-05-20 20:09:02 +0530

    Create main.py

---
+++ Part10/ModelSelection/main.py
@@ -0,0 +1,61 @@
+from sklearn.svm import SVC
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Load data
+df = pd.read_csv("../../Part3/LogisticRegression/Social_Network_Ads.csv")
+
+X = df.iloc[:, :-1].values
+Y = df.iloc[:, -1].values
+
+
+# Split data
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+# Scale Data
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Model
+model = SVC(kernel="rbf", random_state=0)
+model.fit(X_train, Y_train)
+
+# Prediction
+Y_pred = model.predict(X_test)
+
+# Accuracy
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(Y_test, Y_pred)
+print(f"Accuracy: {accuracy}")
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, Y_pred)
+print(cm)
+accuracy_score(Y_test, Y_pred)
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=model, X=X_train, y=Y_train, cv=10,)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, model.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(['#FA8072', '#1E90FF'])(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
